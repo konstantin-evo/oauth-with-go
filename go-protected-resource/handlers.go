f9b3e13c433499fb772cc6e731cefd0c906edac6
konstantin_evo <konstantin.priluchnyi@gmail.com>
2023-08-25 10:48:51 +0300

    Access Token Validation implementation

--- go-protected-resource/handlers.go
+++ go-protected-resource/handlers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type Billing struct {
@@ -15,7 +18,18 @@ type BillingError struct {
 	Error string `json:"error"`
 }
 
-func servicesHandler(w http.ResponseWriter, r *http.Request) {
+type TokenIntrospect struct {
+	Exp    int    `json:"exp"`
+	Nbf    int    `json:"nbf"`
+	Iat    int    `json:"iat"`
+	Jti    string `json:"jti"`
+	Aud    string `json:"aud"`
+	Typ    string `json:"typ"`
+	Acr    string `json:"acr"`
+	Active bool   `json:"active"`
+}
+
+func servicesHandler(w http.ResponseWriter, r *http.Request, app *config) {
 	token, err := extractToken(r)
 	if err != nil {
 		log.Println("Error extracting token:", err)
@@ -26,7 +40,7 @@ func servicesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !validateToken(token) {
+	if !validateToken(token, app) {
 		s := &BillingError{Error: "Invalid token"}
 		encoder := json.NewEncoder(w)
 		encoder.Encode(s)
@@ -52,6 +66,10 @@ func extractToken(r *http.Request) (string, error) {
 	// Try to extract token from Authorization header
 	authHeader := r.Header.Get("Authorization")
 	if authHeader != "" {
+		// Check if the Authorization header starts with "Bearer "
+		if strings.HasPrefix(authHeader, "Bearer ") {
+			return strings.TrimPrefix(authHeader, "Bearer "), nil
+		}
 		return authHeader, nil
 	}
 
@@ -70,6 +88,48 @@ func extractToken(r *http.Request) (string, error) {
 	return "", errors.New("token not found")
 }
 
-func validateToken(token string) bool {
-	return false
+func validateToken(token string, app *config) bool {
+
+	form := url.Values{}
+	form.Add("token", token)
+	form.Add("token_type_hint", "requesting_party_token")
+
+	req, err := http.NewRequest("POST", app.tokenIntrospectURL, strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Println("Error creating a new HTTP request:", err)
+		return false
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(app.clientIntrospect, app.clientSecret)
+
+	// Send the request and handle the response
+	c := &http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		log.Println("Error sending HTTP request:", err)
+		return false
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Println("Status code is not 200:", err)
+		return false
+	}
+
+	// Read and parse the response
+	byteBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Error reading response body:", err)
+		return false
+	}
+
+	tokenIntrospect := &TokenIntrospect{}
+	err = json.Unmarshal(byteBody, tokenIntrospect)
+	if err != nil {
+		log.Println("Error unmarshalling JSON response:", err)
+		return false
+	}
+
+	return tokenIntrospect.Active
 }
